ac6d994438d28123d84c7473837b016e5b25e021
Zin KO <mrzinko.it@gmail.com>
2024-06-08 01:46:56 +0630

    Testing JS query the product.json from github raw

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// Function to fetch products and display them
+async function fetchProducts() {
+  try {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/ZinK0/Your-MALL/main/data/products.json"
+    );
+    const products = await response.json();
+
+    // Display products
+    const productsList = document.getElementById("products-list");
+    products.forEach((product) => {
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("col");
+      productDiv.innerHTML = `
+      <div class="col">
+      <div class="card card_shadow product-card" data-category="${product.category}">
+          <img id="product-img" src="${product.image}" class="card-img-top" alt="${product.name}">
+          <div class="card-body">
+              <h5 class="card-title">${product.name}</h5>
+              <div class="d-flex justify-content-between align-items-center">
+                <small class="text-body-secondary">
+                    <span id="product_price">${product.price}</span>.00 $
+                </small>
+
+                <div class="btn-group">
+                  <button class="fav_add">
+                    <i class="bi bi-bag-plus-fill h3"></i>
+                  </button>
+                  <button class="fav_add">
+                    <i class="bi bi-bag-heart-fill h3"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+      productsList.appendChild(productDiv);
+    });
+  } catch (error) {
+    console.error("Error fetching the products:", error);
+  }
+}
+
+// Call the function to fetch products on page load
+fetchProducts();
