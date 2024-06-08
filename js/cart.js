f278aea764cef681bc3d1ab193f2a49200397818
Zin KO <mrzinko.it@gmail.com>
2024-06-08 23:21:45 +0630

    Added cart page action

--- js/cart.js
+++ js/cart.js
@@ -3,28 +3,25 @@ let cartItems = JSON.parse(localStorage.getItem("cart"));
 if (cartItems) {
   const cartList = document.getElementById("cart-lists");
   cartItems.forEach((cartItem) => {
-    const cartDiv = document.createElement("div");
-    cartDiv.classList.add("col-sm-1", "col-md-3", "col-lg-4", "g-3");
+    const cartDiv = document.createElement("li");
+    // cartDiv.classList.add("col-sm-1", "col-md-3", "col-lg-4", "g-3");
     cartDiv.innerHTML = `
-            <div class="p-3 card card_shadow product-card" data-category="${cartItem.category}">
-                <div class="img_container">
-                    <img id="product-img" src="${cartItem.image}" class="rounded card-img-top" alt="${cartItem.name}">
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">${cartItem.name}</h5>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <small class="text-body-secondary">
-                          <span id="product_price">${cartItem.price}</span>.00 $
-                      </small>
-
-                      <button id="checkout-btn" type="button" class="btn btn-primary">CheckOut</button>
+                    <div class="cart-list-container">
+                        <div class="cart-list-img">
+                            <img width="150px" src="${cartItem.image}" alt="${cartItem.name}"/>
+                            <p>${cartItem.name}</p>
+                        </div>
+                        <div class="cart-list-price>
+                            <p>${cartItem.price}</p>
+                        </div>
                     </div>
-                  </div>
-                </div>
-
             `;
     cartList.appendChild(cartDiv);
-  });
+  }
+const cartBtn = document.getElementById("cart-btn");
+
+
+  );
 } else {
   alert("Your cart is empty!");
   // Redirect to index.html
